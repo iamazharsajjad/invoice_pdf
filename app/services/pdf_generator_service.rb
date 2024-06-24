e84ce62947d826a0393dc717dadbe01e85259f25
iamazharsajjad <iamazharsajjad@gmail.com>
2024-06-24 23:16:58 +0500

    Column Width fixed

--- app/services/pdf_generator_service.rb
+++ app/services/pdf_generator_service.rb
@@ -49,11 +49,11 @@ class PdfGeneratorService
     bill_to_data = [
       [{ content: "Bill To", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right] },
        { content: "Port of Loading", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right] }],
-      ["Name:", "PORT OF LOADING: \t Any Port of Japan"],
-      ["Phone#:", "COUNTRY OF ORIGIN: \t Japan"],
-      ["Email:", "INCOTERMS: \t 2010"],
+      ["Name:", "Port of Loading: \t Any Port of Japan"],
+      ["Phone#:", "Country of Origin: \t Japan"],
+      ["Email:", "Incoterms: \t 2010"],
       ["City:", "P.O.D:"],
-      ["Country:", "COUNTRY:"]
+      ["Country:", "Country:"]
     ]
   
     bill_to_options = {
@@ -87,7 +87,7 @@ class PdfGeneratorService
   end
 
   def vehicle_images
-    image_width = (@document_width - (3 * 20)) / 3  # Considering 20 as the total margin (10 on each side)
+    image_width = (@document_width - (3 * 20)) / 3
   
     # Create a bounding box to hold all three images
     @pdf.bounding_box([0, @pdf.cursor], width: @document_width, height: 90) do
@@ -100,35 +100,35 @@ class PdfGeneratorService
       end
     end
   end
-  
-  
 
   def vehicle_details_section
     vehicle_details_data = [
-      [{ content: "No", background_color: 'D3D3D3', borders:  [:top, :bottom, :left, :right], padding: [3, 5], font_style: :bold, size: 10 }, { content: "Vehicle Details", background_color: 'D3D3D3', align: :center ,borders:  [:top, :bottom, :left, :right], colspan: 6, padding: [3, 5], font_style: :bold, size: 10 }],
-      ["1", "Maker", "", "MONTH / YEAR", "05/2013", "Engine size(cc)", "2000 cc"],
-      ["2", "MODEL", "", "COLOR", "", "No of Seats", "5"],
-      ["3", "MODEL CODE", "DBA-TB17", "Transmission", "AT", "Serial No", ""],
-      ["4", "CHASIS NO", "", "Fuel", "PETROL", "", ""]
+      [{ content: "No", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], padding: [3, 5], font_style: :bold, size: 10 }, 
+       { content: "Vehicle Details", background_color: 'D3D3D3', align: :center, borders: [:top, :bottom, :left, :right], colspan: 6, padding: [3, 5], font_style: :bold, size: 10 }],
+      ["1", "Maker", "", "Month / Year", "05/2013", "Engine size(cc)", "2000 cc"],
+      ["2", "Model", "", "Color", "", "No of Seats", "5"],
+      ["3", "Model Code", "DBA-TB17", "Transmission", "AT", "Serial No", ""],
+      ["4", "Chassis No", "", "Fuel", "Petrol", "", ""]
     ]
   
     vehicle_details_options = {
-      width: @document_width,
+      width: 540,
       row_colors: ['ffffff'],
       cell_style: {
         border_width: 1,
-        borders:  [:top, :bottom, :left, :right],
+        borders: [:top, :bottom, :left, :right],
         border_color: '000000',
         padding: [3, 5]
-      }
+      },
+      column_widths: [30, 80, 80, 80, 90, 90, 90]
     }
   
     @pdf.table(vehicle_details_data, vehicle_details_options) do |table|
-      table.row(0).borders =  [:top, :bottom, :left, :right]
-      table.row(-1).borders =  [:top, :bottom, :left, :right]
+      table.row(0).borders = [:top, :bottom, :left, :right]
+      table.row(-1).borders = [:top, :bottom, :left, :right]
   
-      table.row(0).column(0).borders =  [:top, :bottom, :left, :right]
-      table.row(0).column(1..-1).borders =  [:top, :bottom, :left, :right]
+      table.row(0).column(0).borders = [:top, :bottom, :left, :right]
+      table.row(0).column(1..-1).borders = [:top, :bottom, :left, :right]
   
       table.row(0).font_style = :bold
       table.row(0).background_color = 'D3D3D3'
@@ -136,35 +136,36 @@ class PdfGeneratorService
       table.row(1..-1).size = 8
     end
   end
-
+  
   def payment_details_section
     payment_details_data = [
       [ 
         { content: "", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], font_style: :bold, size: 10 },
-        { content: "Payment Details (Items)", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], colspan: 4, padding: [3, 5], font_style: :bold, size: 10 },
+        { content: "Payment Details (Items)", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], padding: [3, 5], font_style: :bold, size: 10 },
         { content: "Details", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], padding: [3, 5], font_style: :bold, size: 10 },
         { content: "QTY", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], padding: [3, 5], font_style: :bold, size: 10 },
         { content: "AMOUNT", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], padding: [3, 5], font_style: :bold, size: 10 }],
-      ["1", { content: "Car Price", colspan: 4 }, "unit price","1", "$ 0"],
-      ["2", { content: "Local Transportation", colspan: 4 }, "","1", "$ 0"],
-      ["3", { content: "Inspection", colspan: 4 }, "","1", "$ 0"],
-      ["4", { content: "Freight", colspan: 4 }, "","1", "$ 0"],
-      ["5", { content: "Insurance", colspan: 4 }, "","1", "$ 100"],
-      ["6", { content: "Late payment charges", colspan: 4 }, "","1", "$ 0"],
-      ["7", { content: "Demurrage Charges", colspan: 4 }, "","1", "$ 0"],
-      ["8", { content: "Repairing Fee", colspan: 4 }, "","1", "$ 0"],
-      ["9", { content: "Other", colspan: 4 }, "","1", "$ 0"]
+      ["1", { content: "Car Price" }, "Unit Price", "1", "$ 0"],
+      ["2", { content: "Local Transportation" }, "", "1", "$ 0"],
+      ["3", { content: "Inspection" }, "", "1", "$ 0"],
+      ["4", { content: "Freight" }, "", "1", "$ 0"],
+      ["5", { content: "Insurance" }, "", "1", "$ 100"],
+      ["6", { content: "Late payment charges" }, "", "1", "$ 0"],
+      ["7", { content: "Demurrage Charges" }, "", "1", "$ 0"],
+      ["8", { content: "Repairing Fee" }, "", "1", "$ 0"],
+      ["9", { content: "Other" }, "", "1", "$ 0"]
     ]
   
     payment_details_options = {
-      width: @document_width,
+      width: 540,
       row_colors: ['ffffff'],
       cell_style: {
         border_width: 1,
         borders: [:top, :bottom, :left, :right],
         border_color: '000000',
         padding: [3, 5]
-      }
+      },
+      column_widths: [30, 240, 90, 90, 90]
     }
   
     @pdf.table(payment_details_data, payment_details_options) do |table|
@@ -175,32 +176,32 @@ class PdfGeneratorService
       table.row(1..-1).size = 8
     end
   end
-  
+
   def bank_details_section
     bank_details_data = [
       [
-        { content: "BANK DETAILS", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], align: :center, colspan: 4 ,padding: [3, 5], font_style: :bold, size: 10 },
+        { content: "BANK DETAILS", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], colspan: 2, align: :center, padding: [3, 5], font_style: :bold, size: 10 },
         { content: "Discount", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], align: :center, padding: [3, 5], font_style: :bold, size: 10 },
         { content: "$ 0", background_color: 'D3D3D3', borders: [:top, :bottom, :left, :right], align: :center, padding: [3, 5], font_style: :bold, size: 10 }],
-      ["PAYMENT TO", { content: "Sirius Technologies", colspan: 3 }, "Total Payable", { content: "$ 100" }],
-      ["BANK NAME", { content: "Rakuten Bank", colspan: 3 }, "Advance Payable", { content: "$ 0" }],
-      ["BRANCH NAME", { content: "Daiyon eigyou", colspan: 3 }, "Total Paid", { content: "$ 0" }],
-      ["BRANCH CODE", { content: "254", colspan: 3 }, "Remaining Amount", { content: "$ 100" }],
-      ["SWIFT", { content: "RAKTJPJT", colspan: 3 }, "", ""],
-      ["ACCOUNT NO", { content: "7114917", colspan: 3 },"", ""],
+      ["PAYMENT TO", { content: "Sirius Technologies" }, "Total Payable", { content: "$ 100" }],
+      ["BANK NAME", { content: "Rakuten Bank" }, "Advance Payable", { content: "$ 0" }],
+      ["BRANCH NAME", { content: "Daiyon eigyou" }, "Total Paid", { content: "$ 0" }],
+      ["BRANCH CODE", { content: "254" }, "Remaining Amount", { content: "$ 100" }],
+      ["SWIFT", { content: "RAKTJPJT" }, "", ""],
+      ["ACCOUNT NO", { content: "7114917" }, "", ""]
     ]
   
     bank_details_options = {
-      width: @document_width,
+      width: 540,
       row_colors: ['ffffff'],
       cell_style: {
         border_width: 1,
         borders: [:top, :bottom, :left, :right],
         border_color: '000000',
         padding: [3, 5]
-      }
+      },
+      column_widths: [180, 180, 90, 90]
     }
-  
     @pdf.table(bank_details_data, bank_details_options) do |table|
       table.row(0).borders = [:top, :bottom, :left, :right]
       table.row(0).font_style = :bold
@@ -209,15 +210,17 @@ class PdfGeneratorService
       table.column(0).font_style = :bold
       table.column(2).font_style = :bold
       table.column(2).background_color = 'D3D3D3'
-
+  
       table.row(1..-1).size = 8
     end
-
+  
     @pdf.move_down 4
     @pdf.font('Helvetica', size: 10) do
       @pdf.text 'If you have any questions about this invoice, please contact inquiry@otoz.ai', align: :left
     end
   end
+
+ 
 
   def footer
   
